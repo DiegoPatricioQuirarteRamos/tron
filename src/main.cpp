0e97995fd8fa83b0d8ea6889ce40fc61902966bc
DiegoPatricioQuirarteRamos <160290090+DiegoPatricioQuirarteRamos@users.noreply.github.com>
2024-04-23 01:50:22 +0000

    version 2

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,15 @@
 #include <thread>
 #include <fstream>
 #include <string>
+#include <Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    fstream archivo;
-    archivo.open("./assets/images/moto.txt");
-    string moto;
-    while (archivo.good())
-    {
-        archivo >> moto;
-    }
-    archivo.close();
-
-
-    archivo >> moto;
-    archivo.close();
+    Dibujo moto("./assets/images/moto.txt");
+    Dibujo bloque("./assets/images/bloque.txt");
 
     int fotograma = 0;
     while (true)
@@ -29,10 +20,9 @@ int main(int argc, char const *argv[])
     this_thread::sleep_for(0.1s);
     fotograma++;
     Element personaje = spinner (21, fotograma) | bold | color(Color::Red) | bgcolor(Color:: White);
-    Element eMoto = text (moto);
-    Element dibujo = hbox({personaje, eMoto});
+   
+    Element dibujo = vbox({personaje, moto.GetElement(), bloque.GetElement()});
     Screen pantalla = Screen::Create(Dimension::Full());
-
     Render(pantalla, dibujo);
     pantalla.Print();
     pantalla.ResetPosition();
